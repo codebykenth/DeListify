9c79d48e4365b2fe8e5bf337e689411277512429
kenth <kenth@LAPTOP-832N4QBD>
2024-06-18 19:11:25 +0800

    Install and update dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor() {}
-
-  ngOnInit() {
+  isUpdated = false;
+  constructor(private swUpdate: SwUpdate) { }
+  async ngOnInit() {
+    await this.swUpdate.versionUpdates.subscribe(() => {
+      this.swUpdate.checkForUpdate().then(newVersion => {
+        if (newVersion && !this.isUpdated && confirm('A new version is available. Do you want to load it?')) {
+          this.isUpdated = true;
+          window.location.reload();
+        }
+      })
+    })
   }
 }
